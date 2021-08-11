97d0a544f3b934eaf653a894a6a64f7326f04f6d
MiracleWong <cfwr1991@126.com>
2021-08-11 16:16:22 +0800

    add 709 ASCII码转换的解法

--- week08/homework/python/709-to-lower-case.py
+++ week08/homework/python/709-to-lower-case.py
@@ -1,4 +1,16 @@
-# 解法一：内置lower() 函数
+# 解题1：lower() 内置函数
 class Solution:
     def toLowerCase(self, s: str) -> str:
         return s.lower()
+
+# 解题2：ASCII码 转换
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+
+        ansArray = []
+        for i in s:
+            if 65 <= ord(i) <= 90:
+                ansArray.append(chr(ord(i) + 32))
+            else:
+                ansArray.append(i)
+        return ''.join(ansArray)
